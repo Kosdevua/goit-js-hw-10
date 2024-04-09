737bcd854f0daffaadeb199495fe7ea6745c0c74
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-10 00:45:41 +0300

    prepared a project

---
+++ src/js/1-timer.js
@@ -0,0 +1,13 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputElement = document.querySelector('input[type="text"]');
+const buttonElement = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+
+console.log(secondsRef);
